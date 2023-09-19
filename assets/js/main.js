28d70989cbc528461ad24b47b9f465d2d8277de0
HeberRios <68972507+HeberRios@users.noreply.github.com>
2023-09-19 12:08:26 -0600

    Add modal window functionality for the book form
    The necessary overlay for the modal window was added and styled, a
    change in the body height was applied, added the name attribute in the
    form inputs and finally in the main.js file the modal window
    functionality was implemented.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// ELEMENTS --------------------------------------------------------------
+const addBookBtn = document.getElementById("add-book");
+const addBookForm = document.getElementById("add-book-form");
+const overlay = document.getElementById("overlay");
+
+// FUNCTIONS -------------------------------------------------------------
+function toggleBookForm() {
+    addBookForm.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+}
+
+// EVENT LISTENERS -------------------------------------------------------
+
+// TOGGLE BOOK FORM
+addBookBtn.addEventListener("click", toggleBookForm);
+overlay.addEventListener("click", toggleBookForm);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !addBookForm.classList.contains("hidden")) {
+        toggleBookForm();
+    }
+});
